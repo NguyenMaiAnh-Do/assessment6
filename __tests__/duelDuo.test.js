52f1be84661d79bafd8c26d86d536e3ca5737280
Anh Do <anhdodr@gmail.com>
2024-02-28 16:25:29 -0800

    part 4

--- __tests__/duelDuo.test.js
+++ __tests__/duelDuo.test.js
@@ -15,4 +15,22 @@ describe("Duel Duo tests", () => {
     await driver.get("http://localhost:8000");
     await driver.wait(until.titleIs("Duel Duo"), 1000);
   });
+
+
+  test("clicking the Draw button displays the div with id = 'choices'", async () => {
+    await driver.get("http://localhost:8000");
+    // Find and click the Draw button
+    await driver.findElement(By.id("draw")).click();
+    // Wait for the div with id "choices" to be displayed
+    await driver.wait(until.elementLocated(By.id("choices")), 1000);
+  });
+
+  test("clicking an “Add to Duo” button displays the div with id = “player-duo”", async () => {
+    await driver.get("http://localhost:8000");
+    // Find and click the Draw button
+    await driver.findElement(By.id("draw")).click();
+    await driver.findElement(By.className("bot-btn")).click();
+    // Wait for the div with id "choices" to be displayed
+    await driver.wait(until.elementLocated(By.id("player-duo")), 1000);
+  })
 });
